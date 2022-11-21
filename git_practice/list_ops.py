2e1831abf9018eb369064246220c1362568d82e8
Eom Insang <ladder98@sch.ac.kr>
2022-11-21 14:44:24 +0900

    change main to use list ops

---
+++ git_practice/list_ops.py
@@ -0,0 +1,34 @@
+def add_lists(foo, bar):
+    minlen = min(len(foo), len(bar))
+    dst = []
+    for i in range(minlen):
+        dst.append(foo[i] + bar[i])
+    return dst
+    
+def add(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f + b)
+    return out
+
+def subtract(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f - b)
+    return out
+    
+def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
+
+if __name__ == "__main__":
+    dst = add_lists([1,2,3], [4,5,6])
+    print(dst)
